e5f39cab5dbb7c1ae624e255fc75eb2e22699afd
Shafiq <shafiqu@my.centennialcollege.ca>
2018-02-22 00:52:25 -0500

    Smoothed the Camera transition

--- COMP305_001_W2018/Assets/Scripts/CameraMover.cs
+++ COMP305_001_W2018/Assets/Scripts/CameraMover.cs
@@ -18,19 +18,25 @@ public class CameraMover : MonoBehaviour {
 		if(playerPos.transform.position.x > midStart.transform.position.x && 
 			!(playerPos.transform.position.x > safeMidStart.transform.position.x && playerPos.transform.position.x < safeMidEnd.transform.position.x))
 		{
-			//camera follow player's X-position
-			Vector3 newCamPos = new Vector3 (playerPos.transform.position.x, transform.position.y, transform.position.z);
-			this.transform.position = Vector3.MoveTowards (transform.position, newCamPos, step);
+			Vector3 position = transform.position;//grab current cam pos
+			Vector3 newCamPos = new Vector3 (playerPos.transform.position.x, transform.position.y, transform.position.z);//new cam pos will be same as pos of player
+			//this.transform.position = Vector3.MoveTowards (transform.position, newCamPos, step);
+			position = Vector3.Lerp(position, newCamPos, 3.0f * Time.deltaTime);//get a point fraction of the way between current cam pos & new pos
+			transform.position = position;//move cam towards new pos a fraction at a time (per ea frame)
 		}
 
 		if (playerPos.transform.position.x > zoomStart.transform.position.x) {
-			camera.orthographicSize = 5 - (playerPos.transform.position.x - zoomStart.transform.position.x)*0.25f;
+			camera.orthographicSize = 5 - (playerPos.transform.position.x - zoomStart.transform.position.x)*0.2f;
 
 		} 
 
 		if(playerPos.transform.position.x < midStart.transform.position.x) 
 		{
-			this.transform.position = Vector3.MoveTowards (transform.position, cameranitialPos, step);
+			//this.transform.position = Vector3.MoveTowards (transform.position, cameranitialPos, step);
+			Vector3 position = transform.position;//grab current cam pos
+			position = Vector3.Lerp(transform.position, cameranitialPos, 3.0f * Time.deltaTime);
+			transform.position = position;
+
 		}
 
 	}
